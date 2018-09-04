cdc58642c08671fe26cf289d0512a091d40ca7b0
Omar Ahmed <omarahmed1121@gmail.com>
2018-09-03 23:03:48 -0500

    Add Knocikout and build script to call playerstats endpoint

--- PubgStatsWebApp/wwwroot/js/site.js
+++ PubgStatsWebApp/wwwroot/js/site.js
@@ -1,20 +1,40 @@
 ﻿// Write your Javascript code.
 (function () {
 
-    var ModeModel = {
-        KillDeath : ko.observable("").
-        AverageDamage : ko.observable("").
-        WinRation =:ko.observable("")
+    //var ModeModel = {
+    //    KillDeath : ko.observable(""),
+    //    AverageDamage : ko.observable(""),
+    //    WinRation : ko.observable("")
+    //};
+
+    function ModeModel() {
+        this.KillDeath= ko.observable(""),
+        this.AverageDamage= ko.observable(""),
+        this.WinRation= ko.observable("")
     };
 
     var StatsModel = {
-        playerName : ko.observable(""),
+        playerName: ko.observable(""),
         SoloFPP: new ModeModel(),
         DuoFPP: new ModeModel(),
         SquadFPP: new ModeModel(),
         SoloTPP: new ModeModel(),
         DuoTPP: new ModeModel(),
         SquadTPP: new ModeModel()
-    }
+    };
 
+    ko.applyBindings(StatsModel);
+
+
+    function getPlayer(playerName, region) {
+        var base = "http://pubgservicelayer.azurewebsites.net/api/pubg/playerstats/";
+        $.getJSON(base + playerName + "/season/division.bro.official.2018-09/region/" + region, function (data) {
+
+            StatsModel = ko.mapping.fromJS(data);
+        });
+    };
+
+
+    console.log(StatsModel.playerName);
+    
 } ());
