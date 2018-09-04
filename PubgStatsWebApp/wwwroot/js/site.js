e852efa376b80d533e00db22fd5388f46af372e4
Omar Ahmed <omarahmed1121@gmail.com>
2018-09-03 21:51:34 -0500

    Define Knockout Models in script

--- PubgStatsWebApp/wwwroot/js/site.js
+++ PubgStatsWebApp/wwwroot/js/site.js
@@ -1,1 +1,20 @@
 ﻿// Write your Javascript code.
+(function () {
+
+    var ModeModel = {
+        KillDeath : ko.observable("").
+        AverageDamage : ko.observable("").
+        WinRation =:ko.observable("")
+    };
+
+    var StatsModel = {
+        playerName : ko.observable(""),
+        SoloFPP: new ModeModel(),
+        DuoFPP: new ModeModel(),
+        SquadFPP: new ModeModel(),
+        SoloTPP: new ModeModel(),
+        DuoTPP: new ModeModel(),
+        SquadTPP: new ModeModel()
+    }
+
+} ());
